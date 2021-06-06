292b661112d2c68baa12742afb804e5b4525f69c
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-06-06 23:17:18 +0700

    FEAT: add sonar token

--- kero/bin/scan.sh
+++ kero/bin/scan.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+export SONAR_TOKEN="7c5c7374b1d1f09333d9a038039b66739250d275"
 sonar-scanner -Dsonar.organization=drosanda -Dsonar.projectKey=drosanda_seme-indonesian-address-provider-api  -Dsonar.sources=. -Dsonar.host.url=https://sonarcloud.io
